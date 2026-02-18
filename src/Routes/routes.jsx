fd94ede43bf861c8beaf4b5094a78ac2f3c823a5
mdmahfuz307 <njnirob2019@gmail.com>
2026-02-19 03:51:40 +0600

    Search-Page

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import ErrorPage from "../components/ErrorPage.jsx";
 import Home from "../pages/home/Home.jsx";
 import CategoryPage from "../pages/category/CategoryPage.jsx";
+import SearchPage from "../components/SearchPage.jsx";
 
 
 const router = createBrowserRouter([
@@ -13,7 +14,8 @@ const router = createBrowserRouter([
 
     children: [
       { path: "/", element: <Home /> },
-      { path: "/categories/:category", element: <CategoryPage/>}
+      { path: "/categories/:category", element: <CategoryPage/>},
+      { path: "/search", element: <SearchPage/>},
     ],
   },
 ]);
